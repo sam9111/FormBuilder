f023f9e38cdbbe847915810cddd9835b928ec8d7
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-03-13 13:40:02 +0530

    created required field types

---
+++ src/types/types.tsx
@@ -0,0 +1,59 @@
+export type DropdownField = {
+  kind: "dropdown";
+  options: string[];
+  label: string;
+  value: string;
+};
+export type TextField = {
+  kind: "text";
+  label: string;
+  value: string;
+};
+
+export type RadioInputsField = {
+  kind: "radio";
+  options: string[];
+  label: string;
+  value: string;
+};
+
+export type TextAreaField = {
+  kind: "text-area";
+  label: string;
+  value: string;
+};
+
+export type MultiselectField = {
+  kind: "multiselect";
+  options: string[];
+  label: string;
+  value: string[];
+};
+
+export type EmailField = {
+  kind: "email";
+  label: string;
+  value: string;
+};
+
+export type PhoneNumberField = {
+  kind: "phone-number";
+  label: string;
+  value: string;
+};
+
+export type DateField = {
+  kind: "date";
+  label: string;
+  value: string;
+};
+
+export type FormField =
+  | DropdownField
+  | TextField
+  | RadioInputsField
+  | TextAreaField
+  | MultiselectField
+  | EmailField
+  | PhoneNumberField
+  | DateField;
