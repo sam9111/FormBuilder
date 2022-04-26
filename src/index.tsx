09d8015b453d294f26aaa515a80d9e8a988f21d1
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-04-26 17:57:22 +0530

    opt in

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,7 @@ if (process.env.REACT_APP_SENTRY_DSN) {
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
